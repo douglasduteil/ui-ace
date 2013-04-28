9c2f0bf17a87428c8c9dfb84e42f726179b8dd54
Douglas Duteil <douglasduteil@gmail.com>
2013-04-28 21:50:44 +0200

    [FT] Doc test

--- doc/doc.js
+++ doc/doc.js
@@ -1,5 +1,35 @@
-angular.module('doc.ui-ace', ['ui.ace'])
-  .controller('DocCtrl', ['$scope', function($scope){
-    $scope.aceModel = "Ace Hello World";
-  }])
-;
+
+define(['module'],function (module) {
+
+  console.log(module);
+  console.log(module.config());
+  //require(['/modules/directives/ui-codemirror/ui-codemirror.js', 'http://codemirror.net/lib/codemirror.js']);
+
+  /*
+   angular.module('doc.ui-ace', ['ui.ace'])
+   .controller('DocCtrl', ['$scope', function($scope){
+   $scope.aceModel = "Ace Hello World";
+   }])
+   ;
+  requirejs(
+    {
+      paths :  {
+        'ui.codemirror' : root + 'ui-codemirror'
+      },
+      shim : {
+        'ui.codemirror' : { deps: ['http://codemirror.net/lib/codemirror.js'] }
+      }
+    },
+    ['ui.codemirror'],
+    function(){
+
+      angular.module('doc.codeMirror', ['ui.codemirror'])
+        .controller('DocCtrl', ['$scope', function($scope){
+          $scope.codeMirrorModel = "Hello";
+        }])
+      ;
+
+
+    }
+  );  */
+});
